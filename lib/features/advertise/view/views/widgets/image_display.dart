e9d75c554bfcaab44d23542f835da76d2f4d15fb
Assem Ayman <assemayman136@gmail.com>
2024-03-28 13:24:09 +0200

    image picker

---
+++ lib/features/advertise/view/views/widgets/image_display.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:book_extchange/core/utils/measures.dart';
+import 'package:book_extchange/features/advertise/view/view_models/advertise_cubit/advertise_cubit.dart';
+import 'package:book_extchange/features/advertise/view/view_models/advertise_cubit/advertise_state.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+class ImageDisplay extends StatefulWidget{
+ const ImageDisplay({required this.image ,super.key});
+  final File image;
+
+  @override
+  State<ImageDisplay> createState() => _ImageDisplayState();
+}
+
+class _ImageDisplayState extends State<ImageDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AdvertiseCubit,AdvertiseState>(
+      listener: (context, state) => {},
+      builder: (context, state) =>
+       Dismissible (
+        key: Key(widget.image.path),
+        onDismissed: (_)=> AdvertiseCubit.get(context).deleteImage(widget.image) ,
+       child:
+        InkWell(
+            onTap: ()=>
+              AdvertiseCubit.get(context).editImage(widget.image),
+          borderRadius: borderRadius,
+          child: Container(
+            margin: const EdgeInsets.all(16),
+            height: deviceHeight * .3,
+            width: double.infinity,
+            child: ClipRRect(
+              borderRadius: borderRadius,
+              child: Image.file(
+                widget.image,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+       ),
+      ),
+    );
+  }
+}
--- lib/features/advertise/view/views/widgets/images.dart
+++ lib/features/advertise/view/views/widgets/images.dart
@@ -1,28 +1,34 @@
 import 'package:book_extchange/core/widgets/custom_main_button.dart';
 import 'package:book_extchange/features/advertise/view/view_models/advertise_cubit/advertise_cubit.dart';
+import 'package:book_extchange/features/advertise/view/view_models/advertise_cubit/advertise_state.dart';
+import 'package:book_extchange/features/advertise/view/views/widgets/image_adder.dart';
+import 'package:book_extchange/features/advertise/view/views/widgets/image_display.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 class Images extends StatelessWidget {
   const Images({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Text('Images'),
-
-
-        ),
-        CustomMainButton(
-          txt: 'Next',
-          onTap: () {
-            //if (formKey.currentState!.validate()) {}
-
+    return BlocConsumer<AdvertiseCubit, AdvertiseState>(
+      listener: (context, state) => {},
+      builder: (context, state) => Column(
+        children: [
+          ...AdvertiseCubit.get(context).selectedImages.map(
+                (image) => ImageDisplay(
+                  image: image,
+                ),
+              ),
+          const ImageAdder(),
+          CustomMainButton(
+            txt: 'Next',
+            onTap: () {
               AdvertiseCubit.get(context).nextStep(context);
-
-          },
-        )
-      ],
+            },
+          )
+        ],
+      ),
     );
   }
 }
